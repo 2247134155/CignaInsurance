c7c5dbe7d5f19bafabe1dca7cd4eea591a1281e7
Lam Anthony <tony_lam1216@hotmail.com>
2016-12-05 16:55:35 -0500

    Second Commit (Changes made: Added Styles,Image folders, added layout page )

--- CignaInsurance/CignaInsurance/App_Start/RouteConfig.cs
+++ CignaInsurance/CignaInsurance/App_Start/RouteConfig.cs
@@ -13,6 +13,12 @@ namespace CignaInsurance
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
+                name: "HomePage",
+                url: "",
+                defaults: new { controller = "HomePage", action = "Index", id = UrlParameter.Optional }
+                //Anthony Got It
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
