65a2894552c9daa9ac1e1acb8e4ca497ec981d1b
Chris Ge <xiangchenge@gmail.com>
2016-12-06 16:36:31 -0500

    Make actionlink and tempdata work

---
+++ CignaInsurance/CignaInsurance/Controllers/QuoteController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CignaInsurance.Models;
+namespace CignaInsurance.Controllers
+{
+    public class QuoteController : Controller
+    {
+        // GET: Quote
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutQuote()
+        {
+            var customer = (Customer)TempData["Customer"];
+            if (customer == null)
+            {
+                return RedirectToAction("Index","Homepage");
+            }
+            CustomerQuotePlan CustWithQ = new CustomerQuotePlan() { cust = customer,
+                quote = new Quote(),
+                plan = new Plan()};
+            return View();
+        }
+        public ActionResult RetrieveQuote(string id)
+        {
+            var quote = new HealthInsuranceDB().Quotes.Find(id);
+            if (quote == null)
+            {
+                return View();
+            }
+            ViewBag.quotes = quote;
+            return View("AboutQuote", quote.CustomerID);
+        }
+        public ActionResult SaveQuote(CustomerQuotePlan CustWithQ)
+        {
+            if (CustWithQ.quote == null)
+            {
+
+            }
+            else
+            {
+
+            }
+            return View(CustWithQ);
+        }
+        public ActionResult AddPlan(CustomerQuotePlan CustWithQ)
+        {
+            return View(CustWithQ);
+        }
+        
+        public ActionResult Test()
+        {
+            var cust = (Customer)TempData["Customer"];
+            if (cust == null)
+            {
+                return RedirectToAction("Index", "Homepage");
+            }
+            return View(cust);
+        }
+    }
+}
